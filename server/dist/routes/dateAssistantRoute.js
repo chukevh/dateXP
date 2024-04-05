a0badbe6bea7c48ae67424449eee467527051866
Kevin Chu <chu.kevin.h@gmail.com>
2024-04-06 02:35:48 +1100

    Added dockerfiles

--- server/dist/routes/dateAssistantRoute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { callDateActivities, callDateAssistant, callDateSpots } from '../controllers/dateAssistantController.js';
-const router = express.Router();
-router.post("/", callDateAssistant);
-router.post("/spots", callDateSpots);
-router.post("/activities", callDateActivities);
-export default router;
-//# sourceMappingURL=dateAssistantRoute.js.map

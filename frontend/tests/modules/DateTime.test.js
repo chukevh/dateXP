7fed562e64a07b3b4f972a12fec24faf48fb1335
Kevin Chu <chu.kevin.h@gmail.com>
2024-04-23 01:51:54 +1000

    Adding frontend tests

--- frontend/tests/modules/DateTime.test.js
+++ frontend/tests/modules/DateTime.test.js
@@ -1,10 +1,12 @@
-import { render } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import DateTime from "../../src/pages/dashboard/modules/DateTime"
 
 describe("DateTime Component Test", () => {
     it("should allow user to reset time", async () => {
-        const renderedApp = render(<DateTime />)
-        const resetBtn = await renderedApp.getByRole("button", {name: "Reset"})
-
+        render(<DateTime />)
+        const resetBtn = screen.getByRole("button", {name: "Reset"})
+        fireEvent.click(resetBtn)
+        const dateTimer = screen.getByRole("span")
+        console.log(dateTimer)
     })
 })
